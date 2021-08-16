c173af600173b307d38def94dac9e200b1e9c8d3
eko-develops <ekodevelops@gmail.com>
2021-08-16 13:09:56 -0700

    Add get-key-value-posts.php

---
+++ get-key-value-posts.php
@@ -0,0 +1,29 @@
+<?php
+/**Description:
+ * This loop will loop through every post after using get_posts.
+ * When looping through the array, it will also loop through each
+ * single post, and output in a readable form the key => value pairs.
+ */
+
+function get_key_value_pairs(){
+
+    /**You can use get_posts with an array of arguments
+     * to search for posts.
+     */
+    $vendors = get_posts(array(
+        'post_type' => 'POST_TYPE_HERE',
+        'numberposts' => 3
+    ));
+
+    foreach($vendors as $vendor){
+        foreach($vendor as $key => $value){
+            echo $key . " => " . $value . " <br>";
+        }
+        echo "<br>END VENDOR<br><br>";
+    }
+}
+add_action('HOOK_HERE', 'get_key_value_pairs');  //Change HOOK_HERE to wherever you want to display the results
+
+/**References:
+ * To get arguments for get_posts - https://developer.wordpress.org/reference/classes/wp_query/parse_query/
+ */
